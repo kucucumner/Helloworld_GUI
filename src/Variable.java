08354a39fa06d5c36babf71a62d85a6b7cb0d521
kucucumner <kookh0208@naver.com>
2024-04-12 18:30:06 +0900

    変数について勉強した。

---
+++ src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // integer(정수) ... -2, 2, -1243213213
+        System.out.println(a);
+
+        double b = 1.1; // Real Number(실수) ... -2.0, 2.0, 1.8
+        System.out.println(b);
+
+        String c = "Hello World"; // String(문자열)
+        System.out.println(c);
+
+        //
+    }
+}
