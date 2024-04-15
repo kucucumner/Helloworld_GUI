107d70937a70d010ab89927808288a6cbe1b0148
kucucumner <kookh0208@naver.com>
2024-04-15 14:33:16 +0900

    変数の効用

---
+++ src/Letter.java
@@ -0,0 +1,9 @@
+public class Letter {
+    public static void main(String[] args) {
+        String name = "KU"; //변수
+        System.out.println("Hello, "+name+" ... KYOHYUN ... "+name+" ... KU ... bye"); // 변수로 인해 한꺼번에 바뀐다. 변수값은 남들이 알아볼 수 있게 설정한다.
+
+        double VAT = 10.0;
+        System.out.println(VAT); // 이 역시 남들이 봤을 때 코드의 의미를 알 수 있게 작성한다.
+    }
+}
